8edbd8d40979e10cbb4d24eea8de14496fec7d67
Ayoub Echcharrat <ayoub.ech-charrat@hotmail.fr>
2023-06-21 17:42:43 +0200

    Init of the project with ModalPlugin created

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import ModalPlugin from "./lib/ModalPlugin";
+import { createRoot } from 'react-dom/client';
+
+const App = () => {
+    const [toggleModal, setToggleModal] = useState(false)
+
+    return (
+        <div>
+            <h1>Hello React</h1>
+            <button onClick={() => setToggleModal(true)}>Open Modal</button>
+            <ModalPlugin toggleModal={toggleModal} setToggleModal={setToggleModal}
+                colorBG={'#eee'} width={'500px'} height={'300px'} >
+                <h1>Bienvenue !</h1>
+                <p>Votre inscription est validée !</p>
+            </ModalPlugin>
+        </div>
+    );
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
---
+++ src/lib/ModalPlugin.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './ModalPlugin.css'
+
+const ModalPluggin = ({ toggleModal, setToggleModal,children, colorBG, width, height }) => {
+
+  const determinedWidth = width ? width : 'fit-content'
+  const determinedHeight = height ? height : 'fit-content'
+  const determinedColorBG = colorBG ? colorBG : 'white'
+
+  const modalContainerStyle = {
+    width: determinedWidth,
+    height: determinedHeight,
+    backgroundColor: determinedColorBG,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)',
+    borderRadius: '5px',
+    padding:'10px 15px 25px 15px'
+  }
+
+  return (
+    toggleModal &&
+    <div style={modalContainerStyle}>
+      <div onClick={() => setToggleModal(false)} className="close-modal"></div>
+      {children}
+    </div>
+  )
+}
+
+export default ModalPluggin;
